24768ed1043e79e3ef2e8ae93cee88f8fd3b7796
metaraine <raineorshine@gmail.com>
2014-02-23 12:27:05 -0700

    Fix

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 (function() {
+	'use strict';
 
-	var nativity = require('nativity');
+	var nativity = require('nativity'),
+								 cint('cint');
 
 	module.exports = {
 		install: function() {
-			nativity.install(String, host, ['supplant', 'startsWith', 'before', 'after', 'between', 'bookend', { repeatString: 'repeat' }, 'toTitleCase', { strContains: 'contains' }, 'index' ]);
-			nativity.install(Number, host, ['ordinal', { mapNumber: 'map' }]);
-			nativity.install(Array, host, ['each', 'pluck', 'group', 'orderedGroup', 'tally', 'contains', 'unique', 'reversed', 'index', 'rotate', 'toObject', 'find', 'findByProperty', 'filterBy', 'any', 'all', 'spliced', 'shuffle', 'chunk' ]);
-			nativity.install(Function, host, ['any', 'all', 'sequence', 'partialAt', 'arritize', 'toInstance', 'new', 'spy']);
+			nativity.install(String.prototype, cint, ['supplant', 'startsWith', 'before', 'after', 'between', 'bookend', { repeatString: 'repeat' }, 'toTitleCase', { strContains: 'contains' }, 'index' ]);
+			nativity.install(Number.prototype, cint, ['ordinal', { mapNumber: 'map' }]);
+			nativity.install(Array.prototype, cint, ['each', 'pluck', 'group', 'orderedGroup', 'tally', 'contains', 'unique', 'reversed', 'index', 'rotate', 'toObject', 'find', 'findByProperty', 'filterBy', 'any', 'all', 'spliced', 'shuffle', 'chunk' ]);
+			nativity.install(Function.prototype, cint, ['any', 'all', 'sequence', 'partialAt', 'arritize', 'toInstance', 'new', 'spy']);
 		}
 	};
 
