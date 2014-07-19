be640ca701205de17438e3a5ecd5e0180797b5a0
metaraine <raineorshine@gmail.com>
2014-07-19 16:50:20 -0600

    Add inContext.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 			nativity.install(String.prototype, cint, ['supplant', 'startsWith', 'before', 'after', 'between', 'bookend', { repeatString: 'repeat' }, 'toTitleCase', { strContains: 'contains' }, 'index' ]);
 			nativity.install(Number.prototype, cint, ['ordinal', { mapNumber: 'map' }]);
 			nativity.install(Array.prototype, cint, ['group', 'orderedGroup', 'tally', 'contains', 'unique', 'reversed', 'index', 'rotate', 'toObject', 'findByProperty', 'filterBy', 'spliced', 'shuffle', 'chunk' ]);
-			nativity.install(Function.prototype, cint, ['partialAt', 'arritize', 'new', 'spy']);
+			nativity.install(Function.prototype, cint, ['partialAt', 'arritize', 'new', 'spy', 'inContext']);
 		}
 	};
 
